a234f5b0439b87e1cf6545143a0e0e4738a50fdc
rafaelpaniago1 <rafaelpradopaniago@gmail.com>
2025-09-19 12:07:49 -0300

    Bug fixing in driver.py
    User input not being properly recognized.

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 import sys
 from Expression import *
+from Visitor import *
 from Lexer import Lexer
 from Parser import Parser
 
@@ -8,7 +9,18 @@ if __name__ == "__main__":
     Este arquivo nao deve ser alterado, mas deve ser enviado para resolver o
     VPL. O arquivo contem o codigo que testa a implementacao do parser.
     """
-    lexer = Lexer(sys.stdin.read())
+    text = sys.stdin.read()
+    (option, rest) = text.split(maxsplit=1)
+    lexer = Lexer(rest)
     parser = Parser(lexer.tokens())
     exp = parser.parse()
-    print(f"Value is {exp.eval({})}")
+    if option == 'eval':
+        visitor = EvalVisitor()
+        print(f"Value is {exp.accept(visitor, {})}")
+    elif option == 'usedef':
+        visitor = UseDefVisitor()
+        print(f"Are there undefs? {len(exp.accept(visitor, set())) > 0}")
+    elif option == 'safe_eval':
+        safe_eval(exp)
+    else:
+        sys.exit(f"Invalid option = {option}")
